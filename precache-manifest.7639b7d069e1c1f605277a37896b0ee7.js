848f5d5af590a84e85a623e88dca11f7b293ef01
Alan Chen <azc242@nyu.edu>
2020-07-15 10:37:25 -0700

    Updates

---
+++ precache-manifest.7639b7d069e1c1f605277a37896b0ee7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb0804ca696bb08ed0d0c481fb92e6f9",
+    "url": "/libgen-frontend/index.html"
+  },
+  {
+    "revision": "bf98b7fd973f988e17f8",
+    "url": "/libgen-frontend/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3b99d7913bbface5ef48",
+    "url": "/libgen-frontend/static/js/2.e94bb2c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/libgen-frontend/static/js/2.e94bb2c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf98b7fd973f988e17f8",
+    "url": "/libgen-frontend/static/js/main.101642ec.chunk.js"
+  },
+  {
+    "revision": "3cd44bb879cd1653908c",
+    "url": "/libgen-frontend/static/js/runtime-main.b942a0d9.js"
+  }
+]);
